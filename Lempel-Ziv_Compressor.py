8e17bbfb7167dc7c466d21eef3334d2b62a0c36f
Spookyerol <erdalinko@gmail.com>
2019-02-07 18:39:39 +0000

    Created decoder and finished encoder.

--- Lempel-Ziv_Compressor.py
+++ Lempel-Ziv_Compressor.py
@@ -6,24 +6,84 @@ Created on Mon Feb  4 19:19:47 2019
 """
 from bitarray import bitarray
 
-
-
 def encode(msg, W, L):
-    bits = bitarray()
-    msg = msg.zfill(8)
-    print(msg)
+    bitStr = ""
     for char in msg:
-        bits += bin(ord(char))[2:]
-    bits = bits
-    print(bits)
+        bitStr += bin(ord(char))[2:].zfill(8)
+    print(bitStr)
     i = 0
-    while(len(bits) < 0):
-        for j in range(W):
-            for k in range(L):
-                i = i
-        
+    encoded = []
+    while(i <= len(bitStr)-1):
+        codedChar = codeChar(bitStr, i, W, L)
+        encoded.append(codedChar[1])
+        if(i == len(bitStr)-1):
+            break
+        else:
+            i = codedChar[0]
+    print(encoded)
+    return encoded
+
+def codeChar(bitStr, i, W, L):
+    char = bitStr[i]
+    if(i == 0):
+        i = i + 1
+        return i, (0, 0, char)
+    elif(i <= W):
+        windowStart = 0
+    else: 
+        windowStart = i - W
+    if(len(bitStr)-i <= L):
+        bufferEnd = len(bitStr) - 1
+    else:
+        bufferEnd = i + L
+    d = 0
+    l = 0
+    if(bitStr[i] in bitStr[windowStart:i]): 
+        while(bitStr[i] != bitStr[windowStart]):
+            windowStart += 1
+        d = i - windowStart
+        l += 1
+        if(i == len(bitStr)-1):
+            return i, (d, l, "-")
+        while(bitStr[i + l] == bitStr[windowStart + l] and i + l < bufferEnd):
+            l += 1
+        i = i + l
+        char = bitStr[i]
+        return i, (d, l, char)
+    else:
+        i = i + 1
+        return i, (0, 0, char)
     
-encode("abracadabra", 8, 4);
+def decode(codeArr):
+    i = 0
+    j = i
+    bitStr = ""
+    while(codeArr[i][-1] != "-"):
+        #print(codeArr[i])
+        code = codeArr[i]
+        j = i - code[0]
+        #print(j)
+        if(code[1] == 0):
+            bitStr += code[-1]
+        else:
+            for k in range(code[1]):
+                prevCode = codeArr[j]
+                bitStr += prevCode[-1]
+                j = j + 1
+            bitStr += code[-1]
+        i = i + 1
+        if(codeArr[i][-1] == "-"):
+            code = codeArr[i]
+            j = i - code[0]
+            for k in range(code[1]):
+                prevCode = codeArr[j]
+                bitStr += prevCode[-1]
+                j = j + 1
+    
+    #print(bitStr)
+    #bits.tobytes().decode('utf-8')
+        
+decode(encode("abracadabra", 8, 8))
 
 """
 while input is not empty do
